634da7b624cc37aba023e77d210e1b3c215997d9
David Bustos <david@getmonetta.com>
2024-01-17 21:55:34 -0500

    text is deleted after adding the task

--- src/Components/TareaInput.jsx
+++ src/Components/TareaInput.jsx
@@ -4,12 +4,9 @@ import "../styles/tarea-input.css";
 const TareaInput = (props) => {
   const [input, setInput] = useState("");
 
-  const handleChange = (e) => {
-    setInput(e.target.value);
-  };
-
   const handleSubmit = (e) => {
     e.preventDefault();
+    setInput("");
 
     const tareaNueva = {
       id: Math.random(),
@@ -26,7 +23,8 @@ const TareaInput = (props) => {
         type='text'
         placeholder='Type a task'
         name='texto'
-        onChange={handleChange}
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
       />
       <button type='button' className='tarea-button' onClick={handleSubmit}>
         Add Task
